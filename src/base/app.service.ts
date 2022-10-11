dadb9eeba3eacb1549adafd89739f7e79f059b33
Kaipo <kaipojames12@gmail.com>
2022-10-10 22:30:56 -0500

    Update root page content

--- src/base/app.service.ts
+++ src/base/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  root(): any {
+    return {
+      name: 'swgu-nest',
+      description: 'rest-api for Star Wars Galaxy Ultimate',
+      entities: ['Players', 'Toons'],
+    };
   }
 }
