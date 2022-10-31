6e028a4e1da6be7b46ed20f02c8c76737de27685
Kaipo <kaipojames12@gmail.com>
2022-10-30 23:04:38 -0500

    Add route to upgrade playerToon by star

---
+++ src/base/player-toon/util/star-mapper.ts
@@ -0,0 +1,163 @@
+/**
+ * stars: the new level of the Toon
+ * remnants: how many remnants are required to obtain this star level
+ * upgrades: The upgrades a toon will receive after reaching this level
+ */
+const StarMapper = [
+  {
+    stars: 1,
+    remnants: 5,
+    upgrades: {
+      power: 175,
+      speed: 15,
+      attack: 35,
+      defense: 35,
+      health: 650,
+      protection: 50,
+      evasion: 5,
+      criticalChancePercentage: 0,
+      criticalDamagePercentage: 0,
+    },
+  },
+  {
+    stars: 2,
+    remnants: 15,
+    upgrades: {
+      power: 275,
+      speed: 15,
+      attack: 40,
+      defense: 40,
+      health: 690,
+      protection: 60,
+      evasion: 5,
+      criticalChancePercentage: 1,
+      criticalDamagePercentage: 1,
+    },
+  },
+  {
+    stars: 3,
+    remnants: 30,
+    upgrades: {
+      power: 375,
+      speed: 15,
+      attack: 45,
+      defense: 45,
+      health: 750,
+      protection: 64,
+      evasion: 5,
+      criticalChancePercentage: 1,
+      criticalDamagePercentage: 1,
+    },
+  },
+  {
+    stars: 4,
+    remnants: 60,
+    upgrades: {
+      power: 475,
+      speed: 20,
+      attack: 55,
+      defense: 55,
+      health: 865,
+      protection: 72,
+      evasion: 5,
+      criticalChancePercentage: 1,
+      criticalDamagePercentage: 1,
+    },
+  },
+  {
+    stars: 5,
+    remnants: 100,
+    upgrades: {
+      power: 575,
+      speed: 20,
+      attack: 70,
+      defense: 70,
+      health: 916,
+      protection: 78,
+      evasion: 5,
+      criticalChancePercentage: 1,
+      criticalDamagePercentage: 1,
+    },
+  },
+  {
+    stars: 6,
+    remnants: 145,
+    upgrades: {
+      power: 675,
+      speed: 21,
+      attack: 75,
+      defense: 75,
+      health: 1020,
+      protection: 100,
+      evasion: 5,
+      criticalChancePercentage: 1,
+      criticalDamagePercentage: 2,
+    },
+  },
+  {
+    stars: 7,
+    remnants: 190,
+    upgrades: {
+      power: 800,
+      speed: 22,
+      attack: 250,
+      defense: 250,
+      health: 1565,
+      protection: 650,
+      evasion: 5,
+      criticalChancePercentage: 2,
+      criticalDamagePercentage: 3,
+    },
+  },
+  {
+    stars: 8,
+    remnants: 245,
+    upgrades: {
+      power: 900,
+      speed: 23,
+      attack: 265,
+      defense: 265,
+      health: 1754,
+      protection: 767,
+      evasion: 5,
+      criticalChancePercentage: 3,
+      criticalDamagePercentage: 4,
+    },
+  },
+  {
+    stars: 9,
+    remnants: 300,
+    upgrades: {
+      power: 1000,
+      speed: 25,
+      attack: 304,
+      defense: 304,
+      health: 1900,
+      protection: 950,
+      evasion: 10,
+      criticalChancePercentage: 5,
+      criticalDamagePercentage: 6,
+    },
+  },
+  {
+    stars: 10,
+    remnants: 400,
+    upgrades: {
+      power: 1500,
+      speed: 50,
+      attack: 700,
+      defense: 700,
+      health: 2500,
+      protection: 1500,
+      evasion: 50,
+      criticalChancePercentage: 6,
+      criticalDamagePercentage: 7,
+    },
+  },
+];
+
+const getStarMapper = () => {
+  return StarMapper;
+};
+
+export default getStarMapper;
--- src/base/toons/toonDTO.ts
+++ src/base/toons/toonDTO.ts
@@ -1,6 +1,8 @@
+import { Optional } from '@nestjs/common';
 import { ToonMedia } from 'src/objects/toon_media';
 import { ToonStats } from 'src/objects/toon_stats';
 import { Ability } from '../abilities/ability.schema';
+import getStarMapper from '../player-toon/util/star-mapper';
 
 /* Defines The following
     BaseToonDTO
@@ -8,51 +10,91 @@ import { Ability } from '../abilities/ability.schema';
     UpdateToonDTO
 */
 
-class BaseToonDTO {
+export class BaseToonDto {
+  @Optional()
   name: string;
-  shortName: string;
-  uniqueName: string;
-  aliases: string[];
-  tags: string[];
-  stats: ToonStats;
-  media: ToonMedia;
-  unlocked: boolean;
-  remnants: number;
-  stars: number;
-  description: string;
-  abilities: Ability[];
-}
 
-export class CreateToonDto extends BaseToonDTO {
-  constructor(
-    name: string,
-    shortName: string,
-    uniqueName: string,
-    aliases: string[],
-    tags: string[],
-    stats: ToonStats,
-    media: ToonMedia,
-    unlocked: boolean,
-    remnants: number,
-    stars: number,
-    description: string,
-    abilities: Ability[],
-  ) {
-    super();
-    this.name = name;
-    this.shortName = shortName;
-    this.uniqueName = uniqueName;
-    this.aliases = aliases;
-    this.tags = tags;
-    this.stats = stats;
-    this.media = media;
-    this.unlocked = unlocked;
-    this.remnants = remnants;
-    this.stars = stars;
-    this.description = description;
-    this.abilities = abilities;
+  @Optional()
+  shortName: string;
+
+  @Optional()
+  uniqueName: string;
+
+  @Optional()
+  aliases: string[];
+
+  @Optional()
+  tags: string[];
+
+  @Optional()
+  stats: ToonStats;
+
+  @Optional()
+  media: ToonMedia;
+
+  @Optional()
+  unlocked: boolean;
+
+  @Optional()
+  remnants: number;
+
+  @Optional()
+  stars: number;
+
+  @Optional()
+  description: string;
+
+  @Optional()
+  abilities: Ability[];
+
+  constructor(baseToonDto: any) {
+    this.name = baseToonDto?.name;
+    this.shortName = baseToonDto?.shortName;
+    this.uniqueName = baseToonDto?.uniqueName;
+    this.aliases = baseToonDto?.aliases;
+    this.tags = baseToonDto?.tags;
+    this.stats = this.getStartingStats(baseToonDto?.stars, baseToonDto?.stats);
+    this.media = baseToonDto?.media;
+    this.unlocked = baseToonDto?.unlocked;
+    this.stars = baseToonDto?.stars;
+    this.remnants = this.getRemnantStartCount(this.stars);
+    this.description = baseToonDto?.description;
+    this.abilities = baseToonDto?.abilities;
+  }
+
+  getStartingStats(stars: number, baseStats: any) {
+    const statsToAdd = getStarMapper().filter((obj) => obj.stars <= stars);
+    for (const stat of statsToAdd) {
+      baseStats.power += stat.upgrades.power;
+      baseStats.speed += stat.upgrades.speed;
+      baseStats.attack += stat.upgrades.attack;
+      baseStats.defense += stat.upgrades.defense;
+      baseStats.health += stat.upgrades.health;
+      baseStats.protection += stat.upgrades.protection;
+      baseStats.evasion += stat.upgrades.evasion;
+      baseStats.criticalChancePercentage +=
+        stat.upgrades.criticalChancePercentage;
+      baseStats.criticalDamagePercentage +=
+        stat.upgrades.criticalDamagePercentage;
+    }
+    return baseStats;
+  }
+
+  getRemnantStartCount(stars: number) {
+    return getStarMapper().filter((obj) => obj.stars === stars)[0].remnants;
   }
 }
-export class UpdateToonDto extends BaseToonDTO {
+
+export class CreateToonDto extends BaseToonDto {
+  constructor(baseToonDto: any) {
+    super(baseToonDto);
+  }
+}
+export class UpdateToonDto extends BaseToonDto {
+  @Optional()
   completedAt: Date;
+
+  constructor(baseToonDto: any) {
+    super(baseToonDto);
+  }
 }
